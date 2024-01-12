0825ade50edb7a8e45e4b8b880ded9e042e60215
Lompos <mag.play15@gmail.com>
2024-01-12 08:39:05 -0600

    React UseEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Saludar from './components/Saludar';
+
 
 function App() {
 
-  const User = {
-    nombre: "User",
-    edad: 23,
-    color: "Azul"
-  }
 
-  const SaludarFn = (name, edad, color) => {
-    console.log(`Hola ${name} que tal estas? Años: ${edad} y color favorito: ${color}`)
-  }
+  const [stateCar, setStateCar] = useState(false)
+  const [contar, setContarCar] = useState(0)
+const estado = () =>{
+  // setStateCar(!stateCar)
+  setStateCar(preValue => !preValue)
+  setContarCar(contar + 1)
+}
+
+useEffect(() => {
+  console.log("Total " + contar)
+}, [contar])
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Saludar UserInfo = {User} SaludarFn ={SaludarFn} />
-
+        <h1>El carro esta: {stateCar ? "Encendido" : "Apagado"} </h1>
+        <h3>Clicks hechos: {contar}</h3>
+        <button onClick={estado}>Encender o apagar</button>
       </header>
     </div>
   );
