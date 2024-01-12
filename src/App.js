72160b65d9a4bf05b02d4aa72f81d14f5600bb63
Lompos <mag.play15@gmail.com>
2024-01-11 21:55:21 -0600

    React Prueba de Hook UseState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+
+
+  const [stateCar, setStateCar] = useState(false)
+const estado = () =>{
+  // setStateCar(!stateCar)
+  setStateCar(preValue => !preValue)
+}
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>El carro esta: {stateCar ? "Encendido" : "Apagado"} </h1>
+        <button onClick={estado}>Encender o apagar</button>
       </header>
     </div>
   );
 }
 
 export default App;
+//Si no quieres usar div usar fragments que son = <> </> para encapsular varias cosas
+//Usar export function 
